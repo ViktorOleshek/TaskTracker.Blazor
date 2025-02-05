532e612c025ae613db80cbe09e6170740dd0df3a
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2025-02-05 10:48:24 +0200

    Add auth models

--- Domain/Models/LoginModel.cs
+++ Domain/Models/LoginModel.cs
@@ -2,4 +2,6 @@
 
 public class LoginModel
 {
+    public string Login { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
 }
--- Domain/Models/RegisterModel.cs
+++ Domain/Models/RegisterModel.cs
@@ -2,4 +2,8 @@
 
 public class RegisterModel
 {
+    public string Login { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string ConfirmedPassword { get; set; } = string.Empty;
 };
