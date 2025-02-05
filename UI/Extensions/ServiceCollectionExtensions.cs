2608431a36d88d1983fd4fc3bc77e9dfdf24dace
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2025-02-05 14:09:42 +0200

    Add auth service

--- UI/Extensions/ServiceCollectionExtensions.cs
+++ UI/Extensions/ServiceCollectionExtensions.cs
@@ -7,16 +7,16 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddApi(this IServiceCollection services, IConfiguration configuration)
     {
-        //var settings = configuration.GetSection(TaskTrackerSettings.ConfigurationSection)
-        //    .Get<TaskTrackerSettings>();
+        var settings = configuration.GetSection(TaskTrackerSettings.ConfigurationSection)
+            .Get<TaskTrackerSettings>();
 
-        //var types = new [] { typeof(IUserService), typeof(IAuthService) };
+        var types = new [] { typeof(IUserService), typeof(IAuthService) };
 
-        //foreach (var type in types)
-        //{
-        //    services.AddRefitClient(type)
-        //        .ConfigureHttpClient(httpClient => httpClient.BaseAddress = new Uri(settings!.BaseAddress));
-        //}
+        foreach (var type in types)
+        {
+            services.AddRefitClient(type)
+                .ConfigureHttpClient(httpClient => httpClient.BaseAddress = new Uri(settings!.BaseAddress));
+        }
 
         return services;
     }
