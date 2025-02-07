4c8aba0cde61f100b4b957be2d04f7355ff0a211
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2025-02-07 21:53:49 +0200

    Separate Html and C# code

---
+++ UI/Components/Pages/Account/Login.razor.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Models.Account;
+using Microsoft.AspNetCore.Components;
+
+namespace UI.Components.Pages.Account;
+
+public partial class Login : ComponentBase
+{
+    private LoginModel loginModel = new LoginModel();
+    private LoginModelValidation loginModelValidation = new LoginModelValidation();
+    private string? errorMessage;
+    private bool isSubmitting = false;
+
+    private async Task HandleLogin()
+    {
+        isSubmitting = true;
+        errorMessage = null;
+
+        var response = await AuthService.LoginAsync(loginModel);
+
+        if (response.IsSuccessStatusCode)
+        {
+            Console.WriteLine("Login successful!");
+            NavigationManager.NavigateTo("/dashboard");
+        }
+        else
+        {
+            errorMessage = "Invalid data, please try again.";
+        }
+
+        isSubmitting = false;
+    }
+}
---
+++ UI/Components/Pages/Account/Registration.razor.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Models.Account;
+using Microsoft.AspNetCore.Components;
+
+namespace UI.Components.Pages.Account;
+
+public partial class Registration : ComponentBase
+{
+    RegisterModel model = new();
+    RegisterModelValidation registerModelValidation = new();
+
+    private string? errorMessage;
+    private bool isSubmitting = false;
+
+    private async Task HandleRegister()
+    {
+        isSubmitting = true;
+        errorMessage = null;
+
+        var response = await AuthService.RegisterAsync(model);
+
+        if (response.IsSuccessStatusCode && response.Content is not null)
+        {
+            Console.WriteLine($"Successful registration! Token: {response.Content.Token.Token}");
+            NavigationManager.NavigateTo("/dashboard");
+        }
+        else
+        {
+            errorMessage = "Invalid data, please try again.";
+        }
+
+        isSubmitting = false;
+    }
+}
